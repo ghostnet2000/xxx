4cd97208763c181952050db61c76c0e6e9957293
ghostnet2000 <viwelolwane@gmail.com>
2016-09-29 11:45:46 +0200

    changes

---
+++ queries.js
@@ -0,0 +1,100 @@
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://postgres:postgres@localhost:5432/datahub';
+var db = pgp(connectionString);
+
+function getAllMedia(req, res, next) {
+  db.any('select * from ogrgeojson')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL media'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function getSingleMedia(req, res, next) {
+  var pupID = parseStr(req.params.id);
+  db.one('select * from ogrgeojson where ogc_fid = $1', pupID)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ONE media'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function createMedia(req, res, next) {
+  req.body.age = parseInt(req.body.age);
+  db.none('insert into pups(name, breed, age, sex)' +
+      'values(${name}, ${breed}, ${age}, ${sex})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted one media'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function updateMedia(req, res, next) {
+  db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
+    [req.body.name, req.body.breed, parseInt(req.body.age),
+      req.body.sex, parseInt(req.params.id)])
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Updated media'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function removeMedia(req, res, next) {
+  var pupID = parseInt(req.params.id);
+  db.result('delete from pups where id = $1', pupID)
+    .then(function (result) {
+      /* jshint ignore:start */
+      res.status(200)
+        .json({
+          status: 'success',
+          message: `Removed ${result.rowCount} media`
+        });
+      /* jshint ignore:end */
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+
+module.exports = {
+  getAllMedia: getAllMedia,
+  getSingleMedia: getSingleMedia,
+  createMedia: createMedia,
+  updateMedia: updateMedia,
+  removeMedia: removeMedia
+};
